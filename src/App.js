68717af79d3168b411242dfa9f68ac5cdfc0865a
faithcherotich <faithcherotich910@gmail.com>
2024-08-13 22:31:53 +0300

    usenavigate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  // Function to handle login
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  // Function to handle logout
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  // useEffect to handle navigation based on login status
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/"); // Navigate to Home when logged in
+    } else {
+      navigate("/login"); // Navigate to Login when logged out
+    }
+  }, [isLoggedIn]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {/* Conditional rendering based on login status */}
+      {isLoggedIn ? <NavBar logout={logout} /> : <Navigate to="/login" />}
+      <Outlet context={login} />
     </div>
   );
 }
